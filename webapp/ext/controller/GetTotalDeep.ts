717ce1219e84a349453fbea68ea8a408fddcafdb
Mio Yasutake <mio.fujita.01@gmail.com>
2024-12-21 21:03:30 +0000

    deep parameter action

--- webapp/ext/controller/GetTotalDeep.ts
+++ webapp/ext/controller/GetTotalDeep.ts
@@ -1,6 +1,23 @@
 import ExtensionAPI from 'sap/fe/core/ExtensionAPI';
 import Context from 'sap/ui/model/odata/v4/Context';
-import MessageToast from 'sap/m/MessageToast';
+import ListReportExtensionAPI from 'sap/fe/templates/ListReport/ExtensionAPI';
+import ODataContextBinding from 'sap/ui/model/odata/v4/ODataContextBinding';
+import MessageBox from 'sap/m/MessageBox';
+import NumberFormat from 'sap/ui/core/format/NumberFormat';
+
+interface Error {
+	message: string
+	error: { 
+		code: string
+		details: Error[] 
+	}
+}
+
+interface Result {
+    amount: string
+}
+
+const namespace = "com.sap.gateway.srvd.zui_yasu_stock_o4.v0001."
 
 /**
  * Generated event handler.
@@ -9,5 +26,41 @@ import MessageToast from 'sap/m/MessageToast';
  * @param pageContext the context of the page on which the event was fired
  */
 export function getTotalDeep(this: ExtensionAPI, pageContext: Context) {
-    MessageToast.show("Custom handler invoked.");
+    //get selected contexts
+    const ListReportExtensionAPI = this as ListReportExtensionAPI;
+    const contexts = ListReportExtensionAPI.getSelectedContexts();
+    const products = contexts.map(context => {
+        return {
+            productUuId: context.getProperty("Id"),
+            isActiveEntity: context.getProperty("IsActiveEntity")
+        }
+    })
+    
+    contexts[0].getProperty("Id");
+
+    //invoke action
+    const model = ListReportExtensionAPI.getModel();
+    const operation = model?.bindContext("/Stock/" + namespace + "calcStockAmountDeep(...)") as ODataContextBinding;
+    operation.setParameter("dummy", "X");
+    operation.setParameter("_product", products);
+    
+    const fnSuccess = () => {
+        //show total
+        const result = operation.getBoundContext().getObject() as Result;
+        const amount = result.amount;
+        const numberFormat = NumberFormat.getIntegerInstance({
+            groupingEnabled: true
+        });
+        var formattedAmount = numberFormat.format(amount);
+        MessageBox.show(`${formattedAmount} JPY`, {
+            title: "Stock Amount"
+        });
+    }
+
+    const fnError = (error:Error) => {
+        debugger;
+    }
+
+    operation.invoke().then(fnSuccess, fnError);
+
 }
